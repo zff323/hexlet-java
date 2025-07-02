2371ad058f2cdae848df15f42686d507613e5b1c
Fail Zabirov <zff323@mail.ru>
2025-07-02 23:48:24 +0300

    add 4 mod

---
+++ io/hexlet/one.java
@@ -0,0 +1,7 @@
+package io.hexlet;
+public class one {
+	public static void main(String[] args) {
+		two.greeting();
+	}
+}
+
---
+++ io/hexlet/two.java
@@ -0,0 +1,7 @@
+package io.hexlet;
+public class two {
+	public static void greeting() {
+		System.out.println("Java for Brave");
+	}
+}
+
